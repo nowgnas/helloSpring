6fc2a912825f7c965b2de65960396c31baf9515e
nowgnas <marshmello.sw@gmail.com>
2022-03-21 02:37:23 +0900

    mvc pattern

--- src/main/java/dodo/gogo/controller/Hellocontroller.java
+++ src/main/java/dodo/gogo/controller/Hellocontroller.java
@@ -3,13 +3,19 @@ package dodo.gogo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Hellocontroller {
-
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!");
         return "hello"; // hello 파일로 가라
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
